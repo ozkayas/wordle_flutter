def6eaf291c770b19fec90db5708e60d62422587
Suat Özkaya <suatozkaya@gmail.com>
2025-03-08 23:16:39 +0300

    Update dark theme colors for improved contrast and aesthetics

--- lib/core/theme/app_color_theme.dart
+++ lib/core/theme/app_color_theme.dart
@@ -13,9 +13,9 @@ class AppColorTheme extends ThemeExtension<AppColorTheme> {
   AppColorTheme.dark()
       : boxDefault = Colors.blueGrey.shade300,
         boxDark = Colors.blueGrey.shade700,
-        boxYellow = Colors.yellow.shade700,
-        boxGreen = Colors.lightGreen,
-        scaffoldBackground = Colors.black,
+        boxYellow = const Color.fromARGB(255, 209, 160, 37),
+        boxGreen = const Color.fromARGB(255, 143, 182, 98),
+        scaffoldBackground = const Color.fromARGB(255, 19, 19, 19),
         text = Colors.black,
         onBackground = Colors.white;
 
