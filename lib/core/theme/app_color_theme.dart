edf4843eee434217fa51549f61bf3e2271cb7460
Suat Özkaya <suatozkaya@gmail.com>
2025-03-10 23:25:39 +0300

    Add keyboard color properties to AppColorTheme for improved UI consistency

--- lib/core/theme/app_color_theme.dart
+++ lib/core/theme/app_color_theme.dart
@@ -6,9 +6,13 @@ class AppColorTheme extends ThemeExtension<AppColorTheme> {
         boxDark = Color(0XFFa9a9a8),
         boxYellow = Color(0XFFd4c151),
         boxGreen = Color(0xFF15a63b),
+        keyboardEmpty = Color(0XFFe3e3e3),
+        keyboardFalse = Color(0XFFa9a9a8),
         scaffoldBackground = Color(0XFFFCFDFC),
         activeText = Color(0XFFFCFDFC),
         passiveText = Color(0XFFa9a9a8),
+        activeKeyboardText = Colors.white,
+        passiveKeyboardText = Color(0XFF515051),
         onBackground = Colors.black;
 
   AppColorTheme.dark()
@@ -16,7 +20,11 @@ class AppColorTheme extends ThemeExtension<AppColorTheme> {
         boxDark = Color(0XFFa9a9a8),
         boxYellow = Color(0XFFD6BE51),
         boxGreen = Color(0xFF15a63b),
+        keyboardEmpty = Color(0XFF4e4e4f),
+        keyboardFalse = Color(0XFFa9a9a9),
         scaffoldBackground = Color(0XFF383838),
+        activeKeyboardText = Colors.white,
+        passiveKeyboardText = Colors.white,
         activeText = Color(0XFFFCFDFC),
         passiveText = Colors.grey.shade300,
         onBackground = Colors.white;
@@ -26,15 +34,17 @@ class AppColorTheme extends ThemeExtension<AppColorTheme> {
   final Color boxDark;
   final Color boxYellow;
   final Color boxGreen;
+  final Color keyboardEmpty;
+  final Color keyboardFalse;
   final Color scaffoldBackground;
   final Color activeText;
+  final Color activeKeyboardText;
+  final Color passiveKeyboardText;
   final Color passiveText;
   final Color onBackground;
 
   @override
-  ThemeExtension<AppColorTheme> copyWith() {
-    return AppColorTheme.light();
-  }
+  ThemeExtension<AppColorTheme> copyWith() => AppColorTheme.light();
 
   @override
   ThemeExtension<AppColorTheme> lerp(
